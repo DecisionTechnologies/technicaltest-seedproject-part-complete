45aaa690c9bce32bbc38e6812c18b6542469c5ad
Stephen Ayoola <stephen@securebroadcast.com>
2021-01-03 16:52:50 +0000

    test for Criteria 4 and 5 working

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -14,6 +14,7 @@ describe("filter", () => {
     // Assert
     expect(result).toEqual(mockData.deals);
   });
+
   // Test for Criteria 2
   it("should return 4 broadband only deals when filtering by broadband", () => {
     // Arrange
@@ -24,7 +25,8 @@ describe("filter", () => {
     const result = sut.deals
     // Assert
     expect(result.length).toEqual(4)
-  })
+  });
+
   // Test for Criteria 3
   it("should return 4 deals for broadband and tv only when filtering by broadband AND tv", () => {
     // Arrange
@@ -37,6 +39,7 @@ describe("filter", () => {
     // Assert
     expect(result.length).toEqual(4)
   })
+
   // Test for Criteria 4
   it("should return 1 deal for broadband and mobile only when filtering by broadband AND mobile", () => {
     // Arrange
@@ -49,6 +52,20 @@ describe("filter", () => {
     // Assert
     expect(result.length).toEqual(1)
   })
+
+  // Test for Criteria 5
+  it("should return 1 deal for Sky only when filtering by sky", () => {
+    // Arrange
+    const sut = new Store()
+    sut.setDeals(mockData.deals)
+    sut.setProviderFilter(1)
+    // Act
+    const result = sut.deals
+    // Assert
+    expect(result.length).toEqual(1)
+  })
+
+
 
 }
 
