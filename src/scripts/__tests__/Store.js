a573512e27f7c07c966785d6646d061df35ae5a1
Stephen Ayoola <stephen@securebroadcast.com>
2021-01-03 16:41:27 +0000

    test for Criteria 1 and 2 working

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,6 +2,7 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
+  //Filter for Criteria 1
   it("should return all deals when no filters applied", () => {
     // Arrange
     const sut = new Store();
@@ -13,4 +14,18 @@ describe("filter", () => {
     // Assert
     expect(result).toEqual(mockData.deals);
   });
-});
+  // Filter for Criteria 2
+  it("should return 4 broadband only deals when filtering by broadband", () => {
+    // Arrange
+    const sut = new Store()
+    sut.setDeals(mockData.deals)
+    sut.setProductFilter("broadband")
+    // Act
+    const result = sut.deals
+    // Assert
+    expect(result.length).toEqual(4)
+  })
+
+}
+
+);
