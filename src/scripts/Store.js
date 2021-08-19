ea82a45426cbe2c54722d410ccbeafe218abca63
Craig Bojko <craig.bojko@trint.com>
2021-08-19 16:17:36 +0100

    chore: added filter class with product filter function
    - tests passing
    - tested with broadband only filter

--- src/scripts/Store.js
+++ src/scripts/Store.js
@@ -6,7 +6,7 @@ class Store extends Observable {
     this.state = {
       deals: [],
       productFilters: [],
-      providerFilter: null
+      providerFilter: null,
     };
   }
 
@@ -15,7 +15,7 @@ class Store extends Observable {
   }
 
   filter() {
-    return this.state.deals;
+    return new Filter(this.state);
   }
 
   setDeals(data) {
@@ -37,6 +37,75 @@ class Store extends Observable {
   setProviderFilter(value = null) {
     this.state.providerFilter = value;
     this.notify(this.state);
+  }
+}
+
+class Filter {
+  constructor(state) {
+    this.state = state;
+
+    // Ideally this would be a bit more dynamic
+    // get the whole list of filters from a data-layer etc.
+    // - note we're ignore the 'Phone' product type here
+    this.productFilters = ["broadband", "tv", "mobile"];
+
+    // We need to bind the functions to this so we can access the state
+    // within the context of the array filter functions
+    this.filterProduct = this.filterProduct.bind(this);
+    this.filterProvider = this.filterProvider.bind(this);
+
+    return this.filterByState();
+  }
+
+  filterByState() {
+    return this.state.deals
+      .filter(this.filterProduct)
+      .filter(this.filterProvider);
+  }
+
+  filterProduct(deal) {
+    const checkedFilters = this.state.productFilters || [];
+    const uncheckedFilters = this.productFilters.filter(
+      (f) => !checkedFilters.includes(f)
+    );
+
+    /*
+    Handle empty case - nothing selected
+    should return everything
+    */
+    if (checkedFilters.length === 0) {
+      return true;
+    }
+
+    /*
+    Handle filtering by product
+    */
+    // Check if any of the types match our selected filter (.some())
+    // if so - perform logical AND (.every()) across the set to tell us if all the filters are found
+    const inclusionFilter = checkedFilters.every((filter) =>
+      deal.productTypes.some((type) => new RegExp(filter, "i").test(type))
+    );
+
+    // Now check that the deal doesn't include any products we don't have checked
+    // The inner .every() checks that the filter is not matching the productType and this is the case for all the productTypes
+    // The outer .every() logically ANDs the filters together to make sure all the filters are not found
+    const exclusionFilter = uncheckedFilters.every((filter) =>
+      deal.productTypes.every((type) => !new RegExp(filter, "i").test(type))
+    );
+
+    
+    // Finally we need to AND the two results together
+    // Basically if both are True - our filter is a productType,
+    // and no excluded filters are in our deal, then it should be returned
+    return inclusionFilter && exclusionFilter;
+  }
+
+  filterProvider(deal) {
+    // Handle empty case - nothing selected
+    // should return everything
+    if (!this.state.providerFilter) {
+      return true;
+    }
   }
 }
 
